a043098f682467ddc4da25732a9eb49a40923db9
armando bibay jr <armando.bibay@bcstechnology.com.aucelar>
2024-04-07 21:40:20 +0800

    laravel-echo utilization

--- README.md
+++ README.md
@@ -5,3 +5,48 @@
 ```bash
 npm install pusher-js
 ```
+
+## Install Laravel Echo to Listen to Laravel Channels and Events
+
+```bash
+npm install laravel-echo
+```
+
+## Declare all the Pusher credentials on your .env file
+
+.env.local
+
+```env
+ NEXT_PUBLIC_PUSHER_APP_ID=*********
+ NEXT_PUBLIC_PUSHER_APP_KEY=*********
+ NEXT_PUBLIC_PUSHER_APP_SECRET=*********
+ NEXT_PUBLIC_PUSHER_APP_CLUSTER=*********
+```
+
+## create a config file for laravel-echo
+
+<i> configs/laravelEcho.ts</i>
+
+```javascript
+import Echo from "laravel-echo";
+const Pusher = require("pusher-js");
+
+const options = {
+  broadcaster: "pusher",
+  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY || "",
+  cluster: process.env.NEXT_PUBLIC_PUSHER_APP_CLUSTER || "",
+  forceTLS: true,
+};
+
+export const echo = new Echo({
+  ...options,
+});
+```
+
+## set your listeners
+
+```js
+echo.channel("notifications").listen("UserRegistration", (e) => {
+  //do whatever you wanna do
+});
+```
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,21 @@
-import Pusher from "pusher-js";
-import { useEffect, useState } from "react";
+"use client";
 
-const PUSHER_KEY: string = process.env.PUSHER_APP_KEY ?? "";
-const PUSHER_CLUSTER: string = process.env.PUSHER_APP_CLUSTER;
-const NotificationComponent = () => {
+import { useEffect, useState } from "react";
+import { echo } from "./configs/laravelEcho";
+
+function App() {
   const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-    const pusher = new Pusher(PUSHER_KEY, {
-      cluster: PUSHER_CLUSTER,
-      encrypted: true,
+    echo.channel("notifications").listen("UserRegistration", (e) => {
+      setNotifications((prev) => [...prev, ...e]);
     });
-
-    const channel = pusher.subscribe("notifications");
-
-    channel.bind("App\\Events\\NewNotification", (data: any) => {
-      setNotifications((prevNotifications) => [
-        ...prevNotifications,
-        data.message,
-      ]);
-    });
-
-    return () => {
-      channel.unbind_all();
-      channel.unsubscribe();
-    };
   }, []);
 
   return (
     <div>
       <h2>Notifications</h2>
+      <p> </p>
       <ul>
         {notifications.map((notification, index) => (
           <li key={index}>{notification}</li>
@@ -37,6 +23,6 @@ const NotificationComponent = () => {
       </ul>
     </div>
   );
-};
+}
 
-export default NotificationComponent;
+export default App;
